ef283928a1da366fd104cd8660f2ee55e814e7d4
khoivd <khoivd@sudobo.com>
2022-11-24 22:22:46 +0700

    create table users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->tinyInteger('role')->default(4)->comment('1:Admin, 2: BO, 3: Manager, 4: Employee');
+            $table->tinyInteger('status')->default(0)->comment('0: watting, 1: active, 2: deactive');
             $table->string('email')->unique();
+            $table->string('name');
+            $table->date('birthday_at')->nullable();
+            $table->string('address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('cv')->nullable();
+            $table->bigInteger('salary')->nullable();
+            $table->tinyInteger('gender')->default(0)->comment('0: other, 1: male, 2: female');
+            $table->string('phone_number');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
